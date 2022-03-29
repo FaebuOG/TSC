d506cf6031a881ede5d2766f062d5d8dd91adf00
FaebuOG <soly.fabian.g@gmail.com>
2022-03-29 20:27:33 +0200

    Created an Scriptable Object item maker

---
+++ Assets/_TSC/_Scripts/ScriptableObjectsSystem/ItemObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Money,
+    DefaultCard,
+    SpecialCard,
+    Resource,
+    Default
+}
+
+public abstract class ItemObject : ScriptableObject
+{
+    public GameObject prefab;
+
+    public ItemType type;
+    [TextArea(15,20)]
+    public string description;
+
+}
---
+++ Assets/_TSC/_Scripts/ScriptableObjectsSystem/MoneyObject.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New money object", menuName = "Inventory System/Items/Money")]
+public class MoneyObject : ItemObject
+{
+    public int moneyValue;
+    private void Awake()
+    {
+        type = ItemType.Money;
+    }
+}
