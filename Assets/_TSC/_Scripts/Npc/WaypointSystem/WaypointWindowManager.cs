180bfe9c6c0a9191992de6148bc1185d8bc14bc1
Bruno <101364217+Chiori321@users.noreply.github.com>
2022-04-04 22:37:05 +0200

    Waypoint Manager Create waypoint

--- Assets/_TSC/_Scripts/Npc/WaypointSystem/WaypointWindowManager.cs
+++ Assets/_TSC/_Scripts/Npc/WaypointSystem/WaypointWindowManager.cs
@@ -3,17 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class WaypointWindowManager : MonoBehaviour
+public class WaypointWindowManager : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    [MenuItem("Tools/Waypoint Manager")]
+   private static void ShowWindow() 
     {
-        
+        var window = GetWindow<WaypointWindowManager>();
+        window.titleContent = new GUIContent("Waypoint Manager");
+        window.Show();
+    }
+    public Transform WayPointsRoot;
+    private void OnGUI()
+    {
+        var serielizedEditorWindow = new SerializedObject(this);
+
+        EditorGUILayout.PropertyField(serielizedEditorWindow.FindProperty(nameof(WayPointsRoot)));
+
+        if (!WayPointsRoot)
+        {
+            EditorGUILayout.HelpBox("Bitte zuerst Waypoints Root eingeben",MessageType.Info);
+
+        }
+        else
+        {
+            DrawButtons();
+        }
+        serielizedEditorWindow.ApplyModifiedProperties();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DrawButtons() 
     {
-        
+        if (GUILayout.Button("Create Waypoint"))
+        {
+            CreateWayPoint();
+        }
+    }
+    private void CreateWayPoint() 
+    {
+        var waypoint = CreateNewWaypoint();
+
+        Selection.activeGameObject = waypoint.gameObject;
+
+    }
+    private Waypoint CreateNewWaypoint()
+    {
+        var waypointGameObject = new GameObject($"Waypoint{WayPointsRoot.childCount+1}", typeof(Waypoint));
+        waypointGameObject.transform.SetParent(WayPointsRoot.transform, false);
+
+        return waypointGameObject.GetComponent<Waypoint>();
     }
 }
